1a6fc6408875c72afe8d9c755e20050fc6a70135
Neloy Dutta <neloy.dutta@live.com>
2019-05-04 17:58:42 +0530

    adding secret.js

---
+++ secret.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "clientID": "54857794331-un4bakje48tkve1oh33jis0inufrv3o3.apps.googleusercontent.com",
+    "clientSecret": "AoktvzE3IxhbyWRSCmUe9nmM",
+    "dbuser": "todoapp",
+    "dbpassword": "todoapp123"
+}
